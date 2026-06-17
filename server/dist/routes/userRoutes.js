1b1e970f919c86fabd1ccf4fb9338e31d8e385a4
Shubham K <shubhamkbind69@gmail.com>
2026-06-18 01:29:12 +0530

    updated

---
+++ server/dist/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createUserProject, getUserCredits, getUserProject, getUserProjects, purchaseCredits, togglelPublish } from '../controllers/userControllers.js';
+import { protect } from '../middlewares/auth.js';
+const userRouter = express.Router();
+userRouter.get('/credits', protect, getUserCredits);
+userRouter.post('/project', protect, createUserProject);
+userRouter.get('/project/:projectId', protect, getUserProject);
+userRouter.get('/projects', protect, getUserProjects);
+userRouter.get('/publish-toggle/:projectId', protect, togglelPublish);
+userRouter.post('/purchase-credits', protect, purchaseCredits);
+export default userRouter;
